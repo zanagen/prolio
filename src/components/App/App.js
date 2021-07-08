17cb30daf05db1f45c1c80c0ea1316b0fd0ffabc
jess-izuu <izumijessica@gmail.com>
2021-07-07 20:45:11 -0400

    Login Authentication with Firebase: Added Signup, ForgotPassword, PrivateRoute and AuthContext

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,30 +1,36 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Dashboard from '../Dashboard/Dashboard';
 import Preferences from '../Preferences/Preferences';
 import Login from '../Login/Login';
+import { Container } from 'react-bootstrap';
+import { AuthProvider } from '../../contexts/AuthContext';
+import Signup from "../Login/Signup";
+import ForgotPassword from "../Login/ForgotPassword";
+import PrivateRoute from "../Login/PrivateRoute";
+
 
 function App() {
-	const [token, setToken] = useState();
-	if(!token) {
-		return <Login setToken={setToken} />
-	}
-    return (
-	    <div className="wrapper">
-	        <h1>Application</h1>
-	        <BrowserRouter>
-				<Switch>
-					<Route path="/dashboard">
-						<Dashboard />
-					</Route>
-					<Route path="/preferences">
-						<Preferences />
-					</Route>
-				</Switch>
-			</BrowserRouter>
-	    </div>
-	);
+  return (
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <PrivateRoute exact path="/" component={Dashboard} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+              <Route path="/forgot-password" component={ForgotPassword} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
+  )
 }
 
 export default App;
