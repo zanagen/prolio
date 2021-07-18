8196d2854c278d3c3b1ea4995e9cf335583cb434
jess-izuu <izumijessica@gmail.com>
2021-07-18 11:32:09 -0400

    Collapseble navbar with changing screen size and added filter icon, deleted checkboxed to fit Figma Design better, changed padding around buttons

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,11 @@
-// import { Nav, NavLink, NavMenu, Logo } from "./NavbarElements";
 import React, { useState } from "react";
 import { useAuth } from '../../contexts/AuthContext';
 import { useHistory, BrowserRouter as Router } from "react-router-dom";
 import { Logo } from "./NavbarElements";
-import { Nav, NavDropdown, Button, Alert } from "react-bootstrap";
+import { Container, Nav, NavDropdown, Button, Alert } from "react-bootstrap";
 import Navbar from 'react-bootstrap/Navbar';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import SearchBar from "../Searchbar/Searchbar";
-import SearchBarFilter from "../Searchbar/SearchbarFilter";
 import icon from "./icon.png";
 import "./Navbar.css";
 import {
@@ -18,12 +16,21 @@ import {
   faUserFriends,
   faCog,
   faSignOutAlt,
+  faFilter,
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function ProlioNavbar() {
   const [error, setError] = useState("")
   const { currentUser, logout } = useAuth()
   const history = useHistory()
+  const styles = {
+    searchButton: {
+        paddingTop: "0.25rem",
+        paddingBottom: "0.25rem",
+        paddingRight: "0.25rem",
+        paddingLeft: "0.25rem"
+    }
+  }
 
   async function handleLogout() {
     setError("")
@@ -35,69 +42,40 @@ export default function ProlioNavbar() {
       setError("Failed to log out")
     }
   }
+
   return (
     <Router>
-      <Navbar bg="light" variant="light" fixed="top" color="#D3D3D3">
-        {/* <Container> */}
-          <Nav className="me-auto">
-            <Nav.Link href="/Home">
-              <Logo class="logo" src={icon} />
-            </Nav.Link>
-            <Nav.Link href="/Home">
-              <SearchBar />
-              <SearchBarFilter />
-            </Nav.Link>
-            <Nav.Link href="/MyConnections">
-              <FontAwesomeIcon icon={faUserFriends} className="icon1" size = '3x' color="#0E50E3"/>
-            </Nav.Link>
-            <Nav.Link href="/Messages">
-              <FontAwesomeIcon icon={faEnvelope} className="icon2" size = '3x' color="#1954d8"/>
-            </Nav.Link>
-            <NavDropdown title = {<FontAwesomeIcon icon={faUserCircle} size = '3x' color="#1954d8"/>} id="collasible-nav-dropdown">
+      <Navbar expand="xl" bg="light" variant="light" fixed="top" color="#D3D3D3">
+        <Container fluid>
+          <Navbar.Brand href="/home" className="img-container">
+            <Logo class="logo" src={icon} />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <NavDropdown title = {<FontAwesomeIcon icon={faFilter} size = '2x' color="#808080"/>} variant="Secondary" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="/">Following</NavDropdown.Item>
+              <NavDropdown.Item href="/">Recents</NavDropdown.Item>
+              <NavDropdown.Item href="/">Job Posts</NavDropdown.Item>
+              <NavDropdown.Item href="/">Trending</NavDropdown.Item>
+            </NavDropdown>
+            <SearchBar />
+            <Button href="/MyConnections" className="m-1" style={styles.searchButton} variant="outline-primary" size="lg">
+              <FontAwesomeIcon icon={faUserFriends} size = '2x' color="#0E50E3"/>
+            </Button>
+            <Button href="/Messages" className="m-1" style={styles.searchButton} variant="outline-primary" size="lg">
+              <FontAwesomeIcon icon={faEnvelope} size = '2x' color="#1954d8"/>
+            </Button>
+            <NavDropdown title = {<FontAwesomeIcon icon={faUserCircle} size = '2x' color="#1954d8"/>} id="collasible-nav-dropdown">
               <NavDropdown.Item href="/MyProfile"><FontAwesomeIcon icon={faUser} /> MyProfile</NavDropdown.Item>
               <NavDropdown.Item href="/Settings"><FontAwesomeIcon icon={faCog} /> Settings</NavDropdown.Item>
               <NavDropdown.Item href="/Help"><FontAwesomeIcon icon={faQuestionCircle} /> Help</NavDropdown.Item>
               <NavDropdown.Divider />
               {error && <Alert variant="danger">{error}</Alert>}
               <NavDropdown.Item><Button variant="link" onClick={handleLogout}><FontAwesomeIcon icon={faSignOutAlt} /> Logout <strong>{" "}</strong> {currentUser.email}</Button></NavDropdown.Item>
-
-            </NavDropdown>
-          </Nav>
-        {/* </Container> */}
+              </NavDropdown>
+            </Navbar.Collapse>
+          </Container>
         </Navbar>
       </Router>
       )
     }
-    //     <>
-    //       <Nav>
-    //         <NavMenu>
-    //           <NavLink to="/Home" activestyle>
-    //             <Logo src={icon} />
-    //           </NavLink>
-    //           <NavLink to="/Searchfeed" activestyle>
-    //             <SearchBar />
-    //           </NavLink>
-    //           <NavLink to="/MyConnections" activestyle>
-    //             {/*Regular*/}
-    //             <FontAwesomeIcon icon={faBell} />
-    //             {/*Solid*/}
-    //             {/* <FontAwesomeIcon icon={["fas", "user-friends"]} /> */}
-    //           </NavLink>
-    //           <NavLink to="/Messages" activestyle>
-    //             {/*Regular*/}
-    //             <FontAwesomeIcon icon={faEnvelope} />
-    //             {/*Solid*/}
-    //             {/* <FontAwesomeIcon icon={["fas", "envelope"]} /> */}
-    //           </NavLink>
-    //           <NavLink to="/UserProfile" activestyle>
-    //             {/*Regular*/}
-    //             <FontAwesomeIcon icon={faUserCircle} />
-    //             {/*Solid*/}
-    //             {/* <FontAwesomeIcon icon={["fas", "user-circle"]} /> */}
-    //           </NavLink>
-    //         </NavMenu>
-    //       </Nav>
-    //     </>
-    //   );
-    // };
-    
