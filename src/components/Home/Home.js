2a81b3abc65e072ce44ae8b2e5461d5070904f96
zanagen <zanagen@yahoo.com>
2021-07-21 20:19:30 -0700

    standardized Date format
    Dates should be stored in the following format in Firestore: "Fri Dec 5 2019"

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,8 +7,7 @@ import { v4 as uuidv4 } from "uuid";
 import postPic from "../postBackground.png";
 import person from "../person.png";
 
-// .toDateString() --------> Wed Jul 21 2021
-// .toLocaleDateString() --> 7/21/2021
+// .toDateString() --> Wed Jul 21 2021 --> USE THIS WHEN GENERATING NEW POSTS!!!
 
 const pulledData1 = {
     id: uuidv4(),
@@ -19,7 +18,7 @@ const pulledData1 = {
     userStatus: "red",
     postPicSrc: postPic,
     postTitle: "3D animated new york city",
-    postDate: new Date().toDateString(),
+    postDate: "Wed Jul 21 2021",
     postDescription:
         "For the 3D designathon this weekend, my team created a 3-dimensional version on new york city to commemorate stuff.",
     postTags: ["AdobeXD", "Discord", "Slack", "OtherDesigns", "etc."],
@@ -35,7 +34,7 @@ const pulledData2 = {
     userStatus: "green",
     postPicSrc: postPic,
     postTitle: "New 3D Movie",
-    postDate: new Date().toDateString(),
+    postDate: "Mon Mar 22 2021",
     postDescription: "Finished working on a new 3D movie at XXX.",
     postTags: ["3D-Art", "Art", "WorkingFromHome"],
     numLikes: 50
@@ -49,8 +48,8 @@ const pulledData3 = {
     profilePicSrc: person,
     userStatus: "gray",
     postPicSrc: postPic,
-    postTitle: "anime",
-    postDate: new Date().toDateString(),
+    postTitle: "Anime",
+    postDate: "Fri Dec 5 2019",
     postDescription: "Finished support on Anime.",
     postTags: ["2D-Art", "Art", "Contractor"],
     numLikes: 2
